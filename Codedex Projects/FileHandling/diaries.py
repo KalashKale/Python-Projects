da128d7db72d78550433a0cfc323091f5060af62
Kalash Kale <kalashkale0000@gmail.com>
2026-01-12 23:36:25 +0530

    Dear Diary exercise

---
+++ Codedex Projects/FileHandling/diaries.py
@@ -0,0 +1,2 @@
+file = open('diaries.txt', 'w')
+file.write('Hey, this is the Dear Diary exercise from Codedex\nI really enjoy learning on Codedex\nI complete as much courses I can and develop great skills')
