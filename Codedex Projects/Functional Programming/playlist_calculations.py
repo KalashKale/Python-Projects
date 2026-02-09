a2a7c9f201f66e84cc8b8f7b67d58ff5438bd6a3
Kalash Kale <kalashkale0000@gmail.com>
2026-02-10 02:15:20 +0530

    Minutes to seconds using map()

--- Codedex Projects/Functional Programming/playlist_calculations.py
+++ Codedex Projects/Functional Programming/playlist_calculations.py
@@ -7,8 +7,14 @@ def longer_than_five_minutes(song):
     return song[1] > 5.00
 long_songs = filter(longer_than_five_minutes, playlist)
 
-def to_seconds(song):
-    return
+def minutes_to_seconds(song):
+    duration = song[1]
+    minutes = int(duration)
+    seconds = (duration - minutes) * 100
+    total_seconds = (minutes * 60) + seconds
+    return total_seconds
+duration = map(minutes_to_seconds, playlist)
 
-print(f'Playlist: {playlist}')
-print(f'Songs longer than 5 minutes {list(long_songs)}')
+# print(f'Playlist: {playlist}')
+# print(f'Songs longer than 5 minutes {list(long_songs)}')
+print(f'Total duration in seconds: {list(duration)}')
