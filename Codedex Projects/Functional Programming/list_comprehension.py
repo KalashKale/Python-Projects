0260f22787c1887414fe5a0682b2c1003deaa529
Kalash Kale <kalashkale0000@gmail.com>
2026-02-19 22:43:55 +0530

    Even numbers using List comprehension

--- Codedex Projects/Functional Programming/list_comprehension.py
+++ Codedex Projects/Functional Programming/list_comprehension.py
@@ -10,3 +10,9 @@ print(f'Squared numbers: {squares}')
 squares_comprehension = [num ** 2 for num in numbers]
 print(f'Original numbers: {numbers}')
 print(f'Squared numbers using list comprehension: {squares_comprehension}')
+
+numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
+
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(f'\nGiven list: {numbers}')
+print(f'Even numbers from the list: {even_numbers}')
