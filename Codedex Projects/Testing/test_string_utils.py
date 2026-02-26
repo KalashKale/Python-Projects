f401cef0c2b4fc406be8310da3e336bd60354d7f
Kalash Kale <kalashkale0000@gmail.com>
2026-02-26 21:28:17 +0530

    Assertion testing

---
+++ Codedex Projects/Testing/test_string_utils.py
@@ -0,0 +1,15 @@
+import unittest
+from string_utils import reverse_string, capitalize_string, is_capitalized
+
+class TestStringUtils(unittest.TestCase):
+    def test_reverse_string(self):
+        self.assertEqual(reverse_string('hello'), 'olleh')
+
+    def test_capitalize(self):
+        self.assertEqual(capitalize_string('Hello'), 'Hello')
+
+    def test_is_capitalized(self):
+        self.assertTrue(is_capitalized("hello"))
+
+if __name__ == '__main__':
+    unittest.main()
